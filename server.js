ebabc85355ddcd1372e0a543f5468cf6446bf1a9
Matthew Froggatt <froggatt.me@gmail.com>
2016-02-14 16:10:27 -0500

    Initialise repo.

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+  requireAuthentication: function (req, res, next) {
+    console.log('Private route hit!');
+    next();
+  },
+  logger: function (req, res, next) {
+    console.log('Request: ' + new Date().toString() + ' ' +
+      req.method + ' ' + req.originalUrl);
+    next();
+  }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+  res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+  console.log('Express server started using port ' + PORT);
+});
